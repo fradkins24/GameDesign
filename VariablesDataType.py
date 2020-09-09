1b4a8d75a7376504f36e1897ed4f5cb83c36e4d3
Fradkin <fradkins24@greenhill.org>
2020-09-09 14:16:28 -0500

    9/9/2020 These were test runs for the "Types of Variables and Data" homework

---
+++ VariablesDataType.py
@@ -0,0 +1,14 @@
+# Sarah Fradkin
+counter=100
+miles=1000.0
+name="John"
+print(counter)
+print(miles)
+print(name)
+str = "Hello World!"
+print (str)
+print (str[0])
+print (str[2:5])
+print (str[2:])
+print (str * 2)
+print (str + "TEST")
