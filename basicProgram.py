a7821e8d209300cb1f3d19dba849de2448b746d1
Fradkin <fradkins24@greenhill.org>
2020-09-10 19:22:27 -0500

    9/10/2020 Basic programing homework assignment. May need to go back after I know what a modulus is and add one.

---
+++ basicProgram.py
@@ -0,0 +1,14 @@
+# Sarah Fradkin
+x=7
+y=2.0
+a="John"
+c=False
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(c))
+print(x+y)
+print(x-y)
+print(x/y)
+print(x*y)
+print(x%y) #may not need
