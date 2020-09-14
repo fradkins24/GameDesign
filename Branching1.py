4863b24ab0a84be81d1f3e47465762d476d0cfb9
Fradkin <fradkins24@greenhill.org>
2020-09-14 11:39:56 -0500

    9/14/2020 class program

---
+++ Branching1.py
@@ -0,0 +1,18 @@
+# Sarah Fradkin
+# tis is a program to learn how to branch 9/14/2020
+
+# DECLARE VARIABLES
+a=7
+b=13
+c=14
+d=13
+if a%2==0:
+    print ("b+c")
+elif b%2==0:
+    print (c-d)
+else:
+    print (d*c/a)
+#if b%2==0:
+    #print (c-d)
+#else:
+    #print (d*c/a)
---
+++ Looping.py
@@ -0,0 +1,3 @@
+# Sarah Fradkin
+print ("1 2 3 4 5 6 7 8 9")
+print("1\n" + "2\n" + "3\n" + "4\n")
