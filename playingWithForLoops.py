980146448e4fab1f9ee7c35a6e028c39b6165e90
Fradkin <fradkins24@greenhill.org>
2020-09-18 22:27:13 -0500

    9/18/2020 I figured out the prime numbers homework

--- playingWithForLoops.py
+++ playingWithForLoops.py
@@ -1,15 +1,44 @@
 #Sarah Fradkin
 #for number in range(5,0,-1): #goes by third number
-#    print(number)
-#for number in range(4,0,-1):
-#    print(number)
+#    print(number, end = ' ')
 #print ()
+#for number in range(4,0,-1):
+#    print(number, end = ' ')
+#print ()
+#for number in range(3,0,-1):
+#    print(number, end = ' ')
+#print()
+#for number in range(2,0,-1):
+#    print (number, end = ' ')
+#print ()
+#for number in range(1,0,-1):
+#    print(number)
 
 #CLASS loops : HOW TO DO
 
-begin=5
-lines=5 #5 numbers (lines 0,1,2,3,4)
-for line in range(lines): #go from 0 to value
-    for number in range(begin-line,0,-1): #begin(5) minus line, until 0 minus one
-        print(number, end = ' ')
-    print()
+#begin=5
+#lines=5 #5 numbers (lines 0,1,2,3,4)
+#for line in range(lines): #go from 0 to value
+#    for number in range(begin-line,0,-1): #begin(5) minus line, until 0 minus one
+#        print(number, end = ' ')
+#    print()
+
+ #PRIME CHALLENGE ASSIGNMENT
+
+start=25
+end=50
+for number in range(start,end):
+    if number%2==1:
+        if number%3==0:
+            print('',end = '')
+        elif number%5==0:
+            print('',end ='')
+        elif number%7==0:
+            print('',end = '')
+        else:
+            print (number)
+
+# Fibonacci CHALLENGE ASSIGNMENT
+
+#for number in range (10):
+#    print
