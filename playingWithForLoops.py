15f9cbaddc71a89e1ac8824a68f358531da889d7
Fradkin <fradkins24@greenhill.org>
2020-09-23 14:01:30 -0500

    9/23/2020 I added notes to the Fibonacci code

--- playingWithForLoops.py
+++ playingWithForLoops.py
@@ -27,25 +27,25 @@
 
 #start=25
 #end=50
-#for number in range(start,end):
-#    if number%2==1:
-#        if number%3==0:
-#            print('',end = '')
-#        elif number%5==0:
+#for number in range(start,end): # prints for numbers between 25,49
+#    if number%2==1: #if it is odd continue
+#        if number%3==0: # if is divided evenly by three
+#            print('',end = '') #print nothing
+#        elif number%5==0: # if not, then continue to if it divided by 5 evenly and so on...
 #            print('',end ='')
 #        elif number%7==0:
 #           print('',end = '')
 #        else:
-#            print (number)
+#            print (number) # after going through the guidlines, if it passes all og them, the number will be printed and the loop repeats with each number
 
 # Fibonacci CHALLENGE ASSIGNMENT
 
-f1=0
-f2=1
-print (f1,end = ' ')
-print (f2, end = ' ')
-for number in range (0,8):
-    f3=f1+f2
+f1=0 # first number in fibonacci code
+f2=1 # second number in fibonacci code
+print (f1,end = ' ') # prints first number of fibonacci code to start it off
+print (f2, end = ' ') # print second number of fibonacci code to also start it off
+for number in range (0,8): # prints eight number, repeats each time by adding last two numbers
+    f3=f1+f2 # adds last two numbers printed
     print (f3, end = ' ')
     f1=f2 # re-assigns f1
     f2=f3 # re-assigns f2 and repeats loop with new variables
