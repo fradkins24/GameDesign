99dd83ae5fe17e82aef83063dede52e3f9aedb9f
Fradkin <fradkins24@greenhill.org>
2020-09-30 21:15:12 -0500

    9/30/2020 I tried to do the first problem, but haven't figured it out

--- Learning_Strings.py
+++ Learning_Strings.py
@@ -30,12 +30,23 @@
 
 strVar = "Here are the instructions to install Drivers\n" "1. After the download is completed go to where you saved the folder.\n""(By default everything you download from the Internet is saved to the Downloads folder)\n""2. Right click on the folder and choose ''Extract All'' and then choose ''Extract'' again.\n""3. Once all the contents have been extracted you may delete/disregard the folder with the zip icon.\n""4. Next, open and Run the SETUP file. (In most cases it is a setup.exe file OR one listed below:\n""*setup application\n""*Asussetup\n""*pnpinstal64\n""*pnputil\n""*Igxpin\n""5. Please choose to 'repair' or 'update' the existing installation (driver) IF any one of those options do appear during the set up.\n"
 
-# indexDrive=(strVar.find("Drivers")) #trying to find how many times the word Drivers appears
-# print(indexDrive)
+indexDrive=(strVar.find("Drivers")) #trying to find how many times the word Drivers appears
+word_count= 0
+len2=len("Drivers")
+len1=len(strVar)
+i=0
+for i in range(i,len1):
+    if indexDrive > 0:
+        word_count=word_count+1
+        i=indexDrive+len2
+    else:
+        print("Not Found")
+print(word_count)
 
-# print(len(strVar)) #prints leangth of string
+
+#print(len(strVar)) #prints length of string
 #
-# print(strVar.replace('Extract', 'EXTRACT')) #replaces Extract with EXTRACT
+#print(strVar.replace('Extract', 'EXTRACT')) #replaces Extract with EXTRACT
 
 # print(strVar.replace('setup','SETUP')) #replaces setup with setup
 
@@ -45,6 +56,6 @@ strVar = "Here are the instructions to install Drivers\n" "1. After the download
 #indexEnter=(strVar.find("\n")) #finds first \n or first enter
 # print(indexEnter)
 
-index1=(strVar.find("1"))
-indexDot=(strVar.find(".",2))
-print(strVar[index1:indexDot+1])
+# index1=(strVar.find("1")) #trying to print from one until the end of the statement
+# indexDot=(strVar.find("."))
+# print(strVar[index1:indexDot+1])
