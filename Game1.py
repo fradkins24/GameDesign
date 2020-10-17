12cc8935530b08b4629f6d5b0b9b740912af1617
Fradkin <fradkins24@greenhill.org>
2020-10-16 19:09:42 -0500

    10/16/2020 tried some more things

--- Game1.py
+++ Game1.py
@@ -6,6 +6,7 @@ name=input("What is your name?") #asks user to put in name
 print("Good luck", name, end = '' "!") #prints good luck with name
 print() #prints and enter
 gameWords=["pyhton","java","PHP","javaScript","computer","geeks","keyboard","laptop","headphones","hardware","software"] #creates list
+gameWords=["java"]
 #use the choice method of my random function to pick a word
 answer=input("Do you want to guess a word?") #asks user this question
 
@@ -19,15 +20,17 @@ while answer == "yes": #while answer is yes, do following
                 print(char,end=" ") #print the character/letter
             else: #if not
                 print("_",end=" ") #then print a space (_)
+        if word in guesses:
+            turns=0
+            print("Good job! The word was",word,"\nYOU WIN!\n")
         print()
         guess=input("Give me a letter:") #asks user for a letter
         print()
         guesses+=guess #re assigns guesses so that it includes letters already stated
         turns=turns-1 #one less turn
-    # if "_" in word:
+    # if word in guesses:
     #     print("No more tunrs left\nGAME OVER you loose!\n")
     # else:
-    #     turns=0
     #     print("Good job! The word was",word,"\nYOU WIN!\n")
     answer=input("Do you want to play again?")
 
