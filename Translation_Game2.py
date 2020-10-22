e94f03d06b3938659533387dee5421bd94fa5173
Fradkin <fradkins24@greenhill.org>
2020-10-22 15:16:43 -0500

    10/22/2020 I finished the menu

--- Translation_Game2.py
+++ Translation_Game2.py
@@ -12,7 +12,10 @@ def menu():
     print("*    Instructions: Once you choose what type of *\n* word you want to guess, a word in Spanish will*\n* be printed and you will have to write its     *\n* meaning in English. Each time you play, you   *\n* will have three words to define.",space*12,"*")
     print("*",space*45,"*\n*",space*45,"*")
     print("*",space*18,"CHOICES",space*18,"*")
-    print("* If you want to play:", space*24,"*\n*",space,"print the number (1) to guess a number",space*4,"*\n*",space,"print the ")
+    print("* If you want to play:", space*24,"*\n*",space*2,"print the number (1) to guess a number",space*3,"*\n*",space*19,"OR",space*22,"*\n*",space*3,"print the number (2) to guess a verb",space*4,"*")
+    print("*",space*45,"*")
+    print("* If you DO NOT want to play:",space*17,"*\n*",space*2, "print the number (3)",space*21,"*")
+    print(star*25)
 menu()
 # def guess_verb():
 #     h
