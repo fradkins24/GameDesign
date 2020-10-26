57c35224f1cac56bfab6e6081ddaf48061dc3a25
Fradkin <fradkins24@greenhill.org>
2020-10-26 11:36:32 -0500

    10/26/2020 code for when pygame works

---
+++ testingPygame.py
@@ -0,0 +1,11 @@
+#Sarah Fradkin
+import pygame, time, sys
+pygame.init()
+pygame.time.delay(100)
+sreen=pygame.display.set_mode((800,800))
+white=[255,255,255]
+red=[255,0,0]
+green=[0,255,0]
+screen.fill(red)
+pygame.display.set_caption("My game")
+pygame.display.flip
