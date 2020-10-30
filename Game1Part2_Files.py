3dbdcfb4c3d6d11d213dfde79e5c36688fe8e2f6
Fradkin <fradkins24@greenhill.org>
2020-10-30 16:00:54 -0500

    10/30/2020 I added a score

--- Game1Part2_Files.py
+++ Game1Part2_Files.py
@@ -14,60 +14,60 @@ wordsFile.close()
 answer=input("Do you want to guess a fruit? (yes/no) ") #asks user this question
 
 def main():
-    print() #prints and enter
-    head=(" O") #defining body parts
-    larm=(" O\n/|")
-    body=(" O\n |")
-    rarm=(" O\n/|\\")
-    lleg=(" O\n/|\\\n/")
-    rleg=(" O\n/|\\\n/ \\")
-    wordsFile=open("WordsGame1P2.txt","r")
-    word=random.choice(gameWords) #choose a random word from gameWords
-    letters_left=len(word) #creates variable for letters left to guess
-    guesses="" #this is for the guessed characters after the input
-    turns=6 #6 turns bc 6 body parts
-    # print("You have",turns,"turns. Every time you guess wrong another body part will appear and a turn will be reduced.\nOnce the entire body is printed, you loose.\nGood luck", name, end = '' "!\n") #prints good luck with name and says number of turns
-    while turns>0 and letters_left>0: #while there are turns left and letters to guess
-        for char in word: #for characters in the word
-            if char in guesses: #if you guess a charcter in the word
-                print(char,end=" ") #print the character/letter
-            else: #if not
-                print("_",end=" ") #then print a space (_)
-        print() #prints enter after pritning word or _
-        guess=input("Give me a letter:") #asks user for a letter
-        print() #prints enter to seperate each turn
-        count=0 #makes count 0 each time loop repeats
-        if guess[0] not in guesses: #if the guess isn't already in guesses
-            count=word.count(guess[0]) #counts how many times guess appears in word
-            if guess[0] not in word: #if the guess is wrong
-                turns=turns-1 #take away one turn
-        if turns ==5: #for every amount of turns print a body part
-            print(head)
-        if turns==4:
-            print(body)
-        if turns==3:
-            print(larm)
-        if turns==2:
-            print(rarm)
-        if turns==1:
-            print(lleg)
-        if turns==0:
-            print(rleg)
-        letters_left=letters_left-count #sybtracts amount of count from letter left to guess
-        guesses+=guess[0] #re assigns guesses so that it includes letters already stated
-    if letters_left==0: #after the loop end, if uou guessed all the letters
-        print("Good job! The word was",word,end=''".\nYOU WIN!\n") #print that user won
-    else: #if not
-        print("You have 0 turns left.\nGAME OVER YOU LOOSE!\n") #print that they lost
+    score=0
+    for i in range(5):
+        print() #prints and enter
+        head=(" O") #defining body parts
+        larm=(" O\n/|")
+        body=(" O\n |")
+        rarm=(" O\n/|\\")
+        lleg=(" O\n/|\\\n/")
+        rleg=(" O\n/|\\\n/ \\")
+        wordsFile=open("WordsGame1P2.txt","r")
+        word=random.choice(gameWords) #choose a random word from gameWords
+        letters_left=len(word) #creates variable for letters left to guess
+        guesses="" #this is for the guessed characters after the input
+        turns=6 #6 turns bc 6 body parts
+        # print("You have",turns,"turns. Every time you guess wrong another body part will appear and a turn will be reduced.\nOnce the entire body is printed, you loose.\nGood luck", name, end = '' "!\n") #prints good luck with name and says number of turns
+        while turns>0 and letters_left>0: #while there are turns left and letters to guess
+            for char in word: #for characters in the word
+                if char in guesses: #if you guess a charcter in the word
+                    print(char,end=" ") #print the character/letter
+                else: #if not
+                    print("_",end=" ") #then print a space (_)
+            print() #prints enter after pritning word or _
+            guess=input("Give me a letter:") #asks user for a letter
+            print() #prints enter to seperate each turn
+            count=0 #makes count 0 each time loop repeats
+            if guess[0] not in guesses: #if the guess isn't already in guesses
+                count=word.count(guess[0]) #counts how many times guess appears in word
+                if guess[0] not in word: #if the guess is wrong
+                    turns=turns-1 #take away one turn
+            if turns ==5: #for every amount of turns print a body part
+                print(head)
+            if turns==4:
+                print(body)
+            if turns==3:
+                print(larm)
+            if turns==2:
+                print(rarm)
+            if turns==1:
+                print(lleg)
+            if turns==0:
+                print(rleg)
+            letters_left=letters_left-count #sybtracts amount of count from letter left to guess
+            guesses+=guess[0] #re assigns guesses so that it includes letters already stated
+        if letters_left==0: #after the loop end, if uou guessed all the letters
+            print("Good job! The word was",word,end=''".\nYOU WIN!\n") #print that user won
+            score+=1
+        else: #if not
+            print("You have 0 turns left.\nGAME OVER YOU LOOSE!\n") #print that they lost
+    print(score)
 
 while answer == "yes": #while answer is yes, do following
     print("You have 6 turns. Every time you guess wrong another body part will appear and a turn will be reduced.\nOnce the entire body is printed, you loose.\nGood luck", name, end = '' "!\n") #prints good luck with name and says number of turns
     main()
-    print(word)
-    main()
-    main()
-    main()
-    main()
+
     # print() #prints and enter
     # head=(" O") #defining body parts
     # larm=(" O\n/|")
