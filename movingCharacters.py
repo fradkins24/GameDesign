d1c6326c039484436b39cf41d5d212ce8af07d79
fradkins4@greenhill.org <Hornets2839>
2020-11-13 16:02:37 -0600

    11/13/2020 tried to add more code but didn't work for some reason

--- movingCharacters.py
+++ movingCharacters.py
@@ -7,9 +7,10 @@ WIDTH=730
 HIEGHT=475
 x=300
 y=400
-walkR=[pygame.image.load("Sprites\kneR.jpg"),pygame.image.load("Sprites\leapR.jnp")]
-walkL=[pygame.image.load("Sprites\kneeL.jpg"),pygame.image.load("Sprites\leapL.jpg")]
-stand=[pygame.image.load("Sprites\stand.jpg")]
+speed=1
+# walkR=[pygame.image.load("Sprites\kneeR.jpg")]#,pygame.image.load("Sprites\leapR.jnp")]
+# walkL=[pygame.image.load("Sprites\kneeL.jpg"),pygame.image.load("Sprites\leapL.jpg")]
+# stand=[pygame.image.load("Sprites\stand.jpg")]
 screen=pygame.display.set_mode((WIDTH,HIEGHT)) #tuple
 background=pygame.image.load("francepic.png")
 
@@ -17,5 +18,20 @@ pygame.display.set_caption("Testing Characters") #title
 pygame.display.update()
 
 def redrawWindow():
-    screen.blit(background,(0.0))
+    screen.blit(background,(0,0))
     pygame.display.update()
+# while run:
+#     screen.blit(stand,(x,y))
+#     for i in pygame.event.get(): #picks up anything that happens on screen
+#         if i.type == pygame.QUIT: #leaves game
+#             run=False
+#
+#     pressed=pygame.key.get_pressed()
+#
+#     if pressed[pygame.K_RIGHT] and x>0:
+#         pygame.blit(walkR,(x,y))
+#         x-=speed
+#     if pressed[pygame.K_LEFT] and x<WIDTH:
+#         screen.blit(walkL,(x,y))
+#         x+=speed
+    redrawWindow()
