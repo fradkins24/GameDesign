94aecfacec813d2fe8ac2849215fa3e9b8ef7cac
fradkins4@greenhill.org <Hornets2839>
2020-11-14 14:32:45 -0600

    11/14/2020 got the character to move but jumping is delayed when walking

--- movingCharacters.py
+++ movingCharacters.py
@@ -1,37 +1,71 @@
 # Sarah Fradkin
-import pygame
+import pygame, time
 pygame.init()
-
+#defining variables
 run=True
 WIDTH=730
 HIEGHT=475
 x=300
 y=400
-speed=1
-# walkR=[pygame.image.load("Sprites\kneeR.jpg")]#,pygame.image.load("Sprites\leapR.jnp")]
-# walkL=[pygame.image.load("Sprites\kneeL.jpg"),pygame.image.load("Sprites\leapL.jpg")]
-# stand=[pygame.image.load("Sprites\stand.jpg")]
+speed=50
+t=90
+jump=False
+high=10
+#defining characters
+walkR=pygame.image.load("Sprites\kneeR.jpg")
+walkR2=pygame.image.load("Sprites\leapR.jpg")
+walkL=pygame.image.load("Sprites\kneeL.jpg")
+walkL2=pygame.image.load("Sprites\leapL.jpg")
+stand=pygame.image.load("Sprites\stand.jpg")
 screen=pygame.display.set_mode((WIDTH,HIEGHT)) #tuple
 background=pygame.image.load("francepic.png")
+
+w=walkR.get_width() #width of charcater
 
 pygame.display.set_caption("Testing Characters") #title
 pygame.display.update()
 
-def redrawWindow():
+def redrawWindow(): #function for re-setting background and character
     screen.blit(background,(0,0))
     pygame.display.update()
-# while run:
-#     screen.blit(stand,(x,y))
-#     for i in pygame.event.get(): #picks up anything that happens on screen
-#         if i.type == pygame.QUIT: #leaves game
-#             run=False
-#
-#     pressed=pygame.key.get_pressed()
-#
-#     if pressed[pygame.K_RIGHT] and x>0:
-#         pygame.blit(walkR,(x,y))
-#         x-=speed
-#     if pressed[pygame.K_LEFT] and x<WIDTH:
-#         screen.blit(walkL,(x,y))
-#         x+=speed
-    redrawWindow()
+    screen.blit(stand,(x,y))
+    pygame.display.update()
+while run:
+    # screen.blit(stand,(x,y))
+    # pygame.display.update()
+    for i in pygame.event.get(): #picks up anything that happens on screen
+        if i.type == pygame.QUIT: #leaves game
+            run=False
+
+    pressed=pygame.key.get_pressed()
+
+    if pressed[pygame.K_RIGHT]:
+        screen.blit(walkR,(x,y))
+        pygame.display.update()
+        pygame.time.delay(t) #pause to see both chacraters(running)
+        screen.blit(walkR2,(x,y))
+        pygame.display.update()
+        pygame.time.delay(t)
+        if x+w<WIDTH:
+            x+=speed
+    if pressed[pygame.K_LEFT]:
+        screen.blit(walkL,(x,y))
+        pygame.display.update()
+        pygame.time.delay(t) #pause to see both chacraters(running)
+        screen.blit(walkL2,(x,y))
+        pygame.display.update()
+        pygame.time.delay(t)
+        if x>0:
+            x-=speed
+    if not(jump): #jumpimg
+        if pressed[pygame.K_SPACE]:
+            jump=True
+    elif high>=-10:
+        y-=(high*abs(high))/2
+        high-=1
+    else:
+        high=10
+        jump=False
+#BECAUSE OF DELAY ON RUNNING, JUMPIMG IS VERY SLOW
+#ASK HOW TO FIX IT
+    redrawWindow() #calling function
