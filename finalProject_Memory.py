53da7ff668cdd484dba4f0a304c8b5ff3efbfad4
fradkins4@greenhill.org <Hornets2839>
2020-12-07 22:43:45 -0600

    12/7/2020 added more position loops, pygame not working: couldn't test

--- finalProject_Memory.py
+++ finalProject_Memory.py
@@ -12,7 +12,6 @@
 #run game
 import pygame, time, random, math
 
-#setup
 WIDTH=800
 HEIGHT=800
 pygame.init()
@@ -29,6 +28,75 @@ left=200
 down=400
 x=12
 y=12
+
+#defining back of card
+square1=pygame.image.load("images\\backofCard.jpg")
+square1=pygame.transform.scale(square1,(size,size))
+square2=pygame.image.load("images\\backofCard.jpg")
+square2=pygame.transform.scale(square2,(size,size))
+square3=pygame.image.load("images\\backofCard.jpg")
+square3=pygame.transform.scale(square3,(size,size))
+square4=pygame.image.load("images\\backofCard.jpg")
+square4=pygame.transform.scale(square4,(size,size))
+
+
+square5=pygame.image.load("images\\backofCard.jpg")
+square5=pygame.transform.scale(square5,(size,size))
+square6=pygame.image.load("images\\backofCard.jpg")
+square6=pygame.transform.scale(square6,(size,size))
+square7=pygame.image.load("images\\backofCard.jpg")
+square7=pygame.transform.scale(square7,(size,size))
+square8=pygame.image.load("images\\backofCard.jpg")
+square8=pygame.transform.scale(square8,(size,size))
+
+
+square9=pygame.image.load("images\\backofCard.jpg")
+square9=pygame.transform.scale(square9,(size,size))
+square10=pygame.image.load("images\\backofCard.jpg")
+square10=pygame.transform.scale(square10,(size,size))
+square11=pygame.image.load("images\\backofCard.jpg")
+square11=pygame.transform.scale(square11,(size,size))
+square12=pygame.image.load("images\\backofCard.jpg")
+square12=pygame.transform.scale(square12,(size,size))
+
+
+square13=pygame.image.load("images\\backofCard.jpg")
+square13=pygame.transform.scale(square13,(size,size))
+square14=pygame.image.load("images\\backofCard.jpg")
+square14=pygame.transform.scale(square14,(size,size))
+square15=pygame.image.load("images\\backofCard.jpg")
+square15=pygame.transform.scale(square15,(size,size))
+square16=pygame.image.load("images\\backofCard.jpg")
+square16=pygame.transform.scale(square16,(size,size))
+
+
+square17=pygame.image.load("images\\backofCard.jpg")
+square17=pygame.transform.scale(square17,(size,size))
+square18=pygame.image.load("images\\backofCard.jpg")
+square18=pygame.transform.scale(square18,(size,size))
+square19=pygame.image.load("images\\backofCard.jpg")
+square19=pygame.transform.scale(square19,(size,size))
+square20=pygame.image.load("images\\backofCard.jpg")
+square20=pygame.transform.scale(square20,(size,size))
+
+square21=pygame.image.load("images\\backofCard.jpg")
+square21=pygame.transform.scale(square21,(size,size))
+square22=pygame.image.load("images\\backofCard.jpg")
+square22=pygame.transform.scale(square22,(size,size))
+square23=pygame.image.load("images\\backofCard.jpg")
+square23=pygame.transform.scale(square23,(size,size))
+square24=pygame.image.load("images\\backofCard.jpg")
+square24=pygame.transform.scale(square24,(size,size))
+
+square25=pygame.image.load("images\\backofCard.jpg")
+square25=pygame.transform.scale(square25,(size,size))
+square26=pygame.image.load("images\\backofCard.jpg")
+square26=pygame.transform.scale(square26,(size,size))
+square27=pygame.image.load("images\\backofCard.jpg")
+square27=pygame.transform.scale(square27,(size,size))
+square28=pygame.image.load("images\\backofCard.jpg")
+square28=pygame.transform.scale(square28,(size,size))
+
 
 #colors
 white=(255,255,255)
@@ -54,21 +122,21 @@ matches1=[pygame.image.load("images\\panda.jpg"),pygame.image.load("images\\pand
 random.shuffle(matches1)
 define1={}
 for number in range(12):
-    define1["e{0}".format(number)]=matches1[number]
+    define1["b{0}".format(number)]=matches1[number]
 
 #medium
 matches2=[pygame.image.load("images\\panda.jpg"),pygame.image.load("images\\panda.jpg"),pygame.image.load("images\\bat.jpg"),pygame.image.load("images\\bat.jpg"),pygame.image.load("images\\elephant.jpg"),pygame.image.load("images\\elephant.jpg"),pygame.image.load("images\\giraffe.jpg"),pygame.image.load("images\\giraffe.jpg"),pygame.image.load("images\\hippo.jpg"),pygame.image.load("images\\hippo.jpg"),pygame.image.load("images\\lion.jpg"),pygame.image.load("images\\lion.jpg"),pygame.image.load("images\\monkey.jpg"),pygame.image.load("images\\monkey.jpg"),pygame.image.load("images\\snake.jpg"),pygame.image.load("images\\snake.jpg"),pygame.image.load("images\\turtle.jpg"),pygame.image.load("images\\turtle.jpg"),pygame.image.load("images\\zebra.jpg"),pygame.image.load("images\\zebra.jpg")]
 random.shuffle(matches2)
 define2={}
 for number in range(20):
-    define2["m{0}".format(number)]=matches2[number]
+    define2["b{0}".format(number)]=matches2[number]
 
 #hard
 matches3=[pygame.image.load("images\\panda.jpg"),pygame.image.load("images\\panda.jpg"),pygame.image.load("images\\bat.jpg"),pygame.image.load("images\\bat.jpg"),pygame.image.load("images\\elephant.jpg"),pygame.image.load("images\\elephant.jpg"),pygame.image.load("images\\giraffe.jpg"),pygame.image.load("images\\giraffe.jpg"),pygame.image.load("images\\hippo.jpg"),pygame.image.load("images\\hippo.jpg"),pygame.image.load("images\\lion.jpg"),pygame.image.load("images\\lion.jpg"),pygame.image.load("images\\monkey.jpg"),pygame.image.load("images\\monkey.jpg"),pygame.image.load("images\\snake.jpg"),pygame.image.load("images\\snake.jpg"),pygame.image.load("images\\turtle.jpg"),pygame.image.load("images\\turtle.jpg"),pygame.image.load("images\\zebra.jpg"),pygame.image.load("images\\zebra.jpg"),pygame.image.load("images\\bee.jpg"),pygame.image.load("images\\bee.jpg"),pygame.image.load("images\\ladybug.jpg"),pygame.image.load("images\\ladybug.jpg"),pygame.image.load("images\\crab.jpg"),pygame.image.load("images\\crab.jpg"),pygame.image.load("images\\starfish.jpg"),pygame.image.load("images\\starfish.jpg"),]
 random.shuffle(matches3)
 define3={}
 for number in range(28):
-    define3["h{0}".format(number)]=matches3[number]
+    define3["b{0}".format(number)]=matches3[number]
 
 # pics0=pygame.transform.scale(define1["e0"],(110,110))
 # pics1=pygame.transform.scale(define1["e1"],(110,110))
@@ -84,70 +152,33 @@ for number in range(28):
 def draw():
     global score, square1, square2, square3, square4, square5, square6, square7, square8, square9, square10, square11, square12, square13, square14, square15, square16, square17, square18, square19, square20, square21, square22, square23, square24, square25, square26, square27, square28
     screen.fill(black)
+    up9=True
     #for hard
-    # square1=pygame.image.load("images\\backofCard.jpg")
-    # square1=pygame.transform.scale(square1,(size,size))
+
     # screen.blit(square1,(x,y))
-    # square2=pygame.image.load("images\\backofCard.jpg")
-    # square2=pygame.transform.scale(square2,(size,size))
     # screen.blit(square2,(x+margin,y))
-    # square3=pygame.image.load("images\\backofCard.jpg")
-    # square3=pygame.transform.scale(square3,(size,size))
     # screen.blit(square3,(x+margin*2,y))
-    # square4=pygame.image.load("images\\backofCard.jpg")
-    # square4=pygame.transform.scale(square4,(size,size))
     # screen.blit(square4,(x+margin*3,y))
-    #
-    # square5=pygame.image.load("images\\backofCard.jpg")
-    # square5=pygame.transform.scale(square5,(size,size))
+
     # screen.blit(square5,(x,y+margin))
-    # square6=pygame.image.load("images\\backofCard.jpg")
-    # square6=pygame.transform.scale(square6,(size,size))
     # screen.blit(square6,(x+margin,y+margin))
-    # square7=pygame.image.load("images\\backofCard.jpg")
-    # square7=pygame.transform.scale(square7,(size,size))
     # screen.blit(square7,(x+margin*2,y+margin))
-    # square8=pygame.image.load("images\\backofCard.jpg")
-    # square8=pygame.transform.scale(square8,(size,size))
     # screen.blit(square8,(x+margin*3,y+margin))
 
-    square9=pygame.image.load("images\\backofCard.jpg")
-    square9=pygame.transform.scale(square9,(size,size))
-    screen.blit(square9,(x,y+margin*2))
-    square10=pygame.image.load("images\\backofCard.jpg")
-    square10=pygame.transform.scale(square10,(size,size))
+    while up9:
+        screen.blit(square9,(x,y+margin*2))
     screen.blit(square10,(x+margin,y+margin*2))
-    square11=pygame.image.load("images\\backofCard.jpg")
-    square11=pygame.transform.scale(square11,(size,size))
     screen.blit(square11,(x+margin*2,y+margin*2))
-    square12=pygame.image.load("images\\backofCard.jpg")
-    square12=pygame.transform.scale(square12,(size,size))
     screen.blit(square12,(x+margin*3,y+margin*2))
 
-    square13=pygame.image.load("images\\backofCard.jpg")
-    square13=pygame.transform.scale(square13,(size,size))
     screen.blit(square13,(x,y+margin*3))
-    square14=pygame.image.load("images\\backofCard.jpg")
-    square14=pygame.transform.scale(square14,(size,size))
     screen.blit(square14,(x+margin,y+margin*3))
-    square15=pygame.image.load("images\\backofCard.jpg")
-    square15=pygame.transform.scale(square15,(size,size))
     screen.blit(square15,(x+margin*2,y+margin*3))
-    square16=pygame.image.load("images\\backofCard.jpg")
-    square16=pygame.transform.scale(square16,(size,size))
     screen.blit(square16,(x+margin*3,y+margin*3))
 
-    square17=pygame.image.load("images\\backofCard.jpg")
-    square17=pygame.transform.scale(square17,(size,size))
     screen.blit(square17,(x,y+margin*4))
-    square18=pygame.image.load("images\\backofCard.jpg")
-    square18=pygame.transform.scale(square18,(size,size))
     screen.blit(square18,(x+margin,y+margin*4))
-    square19=pygame.image.load("images\\backofCard.jpg")
-    square19=pygame.transform.scale(square19,(size,size))
     screen.blit(square19,(x+margin*2,y+margin*4))
-    square20=pygame.image.load("images\\backofCard.jpg")
-    square20=pygame.transform.scale(square20,(size,size))
     screen.blit(square20,(x+margin*3,y+margin*4))
 
     # for column in range(0,400,size+margin):
@@ -163,7 +194,7 @@ def draw():
     pygame.display.update()
 
 #main code
-def main():
+def main(dict):
     global score, square1, square2, square3, square4, square5, square6, square7, square8, square9, square10, square11, square12, square13, square14, square15, square16, square17, square18, square19, square20, square21, square22, square23, square24, square25, square26, square27, square28
     score=str(0)
     run=True
@@ -176,9 +207,70 @@ def main():
                     pos=pygame.mouse.get_pos()
                     pos=pos[0]//(size+x),pos[1]//(size+y)
                     if pos[0]==0 and pos[1]==2:
-                        print("yes!")
-        draw()
+                        pic0=pygame.transform.scale(dict["b0"],(size,size))
+                        screen.blit(pic0,(x,y+margin*2))
+                        #up9=False
+                        # pygame.display.update()
+                        # pygame.time.delay(1000)
+                    if pos[0]==1 and pos[1]==2:
+                        pic1=pygame.transform.scale(dict["b1"],(size,size))
+                        screen.blit(pic1,(x+margin,y+margin*3))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+                    if pos[0]==2 and pos[1]==2:
+                        pic2=pygame.transform.scale(dict["b2"],(size,size))
+                        screen.blit(pic2,(x+margin*2,y+margin*3))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+                    if pos[0]==3 and pos[1]==2:
+                        pic3=pygame.transform.scale(dict["b3"],(size,size))
+                        screen.blit(pic3,(x+margin*3,y+margin*3))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
 
+                    if pos[0]==0 and pos[1]==3:
+                        pic4=pygame.transform.scale(dict["b4"],(size,size))
+                        screen.blit(pic4,(x,y+margin*4))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+                    if pos[0]==1 and pos[1]==3:
+                        pic5=pygame.transform.scale(dict["b5"],(size,size))
+                        screen.blit(pic5,(x+margin,y+margin*4))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+                    if pos[0]==2 and pos[1]==3:
+                        pic6=pygame.transform.scale(dict["b6"],(size,size))
+                        screen.blit(pic6,(x+margin*2,y+margin*4))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+                    if pos[0]==3 and pos[1]==3:
+                        pic7=pygame.transform.scale(dict["b7"],(size,size))
+                        screen.blit(pic7,(x+margin*3,y+margin*4))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+
+                    if pos[0]==0 and pos[1]==4:
+                        pic8=pygame.transform.scale(dict["b8"],(size,size))
+                        screen.blit(pic8,(x,y+margin*5))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+                    if pos[0]==1 and pos[1]==4:
+                        pic9=pygame.transform.scale(dict["b9"],(size,size))
+                        screen.blit(pic9,(x+margin,y+margin*5))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+                    if pos[0]==2 and pos[1]==4:
+                        pic10=pygame.transform.scale(dict["b10"],(size,size))
+                        screen.blit(pic10,(x+margin*2,y+margin*5))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+                    if pos[0]==3 and pos[1]==4:
+                        pic11=pygame.transform.scale(dict["b11"],(size,size))
+                        screen.blit(pic11,(x+margin*3,y+margin*5))
+                        pygame.display.update()
+                        pygame.time.delay(1000)
+        draw()
+    pygame.display.update()
 #menu
 def menu():
     runMenu=True
@@ -271,4 +363,4 @@ def menu():
 
 
         pygame.display.update()
-main()
+main(define1)
