c2a677ae57f858fc44fdeb0c2ee9094db7b8b84e
fradkins4@greenhill.org <Hornets2839>
2020-12-09 23:17:24 -0600

    12/9/2020 don't know why, but when match in medium rows different value

--- finalProject_Memory.py
+++ finalProject_Memory.py
@@ -413,12 +413,12 @@ def main(dict):
                     if count==2:
                         score+=1
                         if clicked[0]==clicked[1]:
-                            #print("yes")
+                            print(clicked)
                             clicked=[]
                             count=0
                             tracking=[]
                         else:
-                            #print("no")
+                            print(clicked)
                             clicked=[]
                             count=0
                             pygame.time.delay(800)
